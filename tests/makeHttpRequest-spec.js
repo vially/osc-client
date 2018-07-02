31aec84096f901a6c3c63d1abd381faca6dd53d3
Valentin Hăloiu <valentin.haloiu@areo.io>
2018-07-02 16:48:10 +0200

    Replace superagent with fetch

--- tests/makeHttpRequest-spec.js
+++ tests/makeHttpRequest-spec.js
@@ -2,10 +2,11 @@
 
 var makeHttpRequest = require('../lib/makeHttpRequest')
 var assert = require('assert')
+var fetch = require('cross-fetch')
 
 describe('makeHttpRequest', function () {
   it('should return a promise', function () {
-    var prom = makeHttpRequest('GET', 'http://localhost/')
+    var prom = makeHttpRequest(fetch, 'GET', 'http://localhost/')
     assert(typeof prom.then === 'function')
     assert(typeof prom.catch === 'function')
   })
